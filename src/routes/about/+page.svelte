797a86391f57cb133b1a3f8f292790a68db262d6
markmybytes <markmybytes@gmail.com>
2025-02-13 16:01:58 +0800

    Add about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+</script>
+
+<div class="p-2">
+	<div class="flex flex-col gap-y-4 rounded bg-white p-2">
+		<div class="flex flex-col gap-y-2">
+			<p>迷失港鐵是一個車廂位置定位工具。透過車廂編號及車門編號來尋找你所在的位置。</p>
+			<p>而此工具與港鐵是完全沒有關係。</p>
+		</div>
+
+		<hr class="my-3 border-gray-300" />
+
+		<div class="flex">
+			<p class="w-1/2">源碼／匯報問題</p>
+			<a href="https://github.com/SuperDumbTM/lost-in-mtr" class="content-center">🔗 Github </a>
+		</div>
+
+		<div class="flex">
+			<p class="w-1/2">編組資料來源</p>
+			<a href="https://hkrail.fandom.com/wiki/%E9%A6%96%E9%A0%81" class="content-center"
+				>🔗 香港鐵路大典
+			</a>
+		</div>
+	</div>
+</div>
