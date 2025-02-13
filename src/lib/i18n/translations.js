6244a6b702c3561f6b1b82c296b8207638f2ef94
markmybytes <markmybytes@gmail.com>
2025-02-13 16:01:32 +0800

    Update translation files loading method

--- src/lib/i18n/translations.js
+++ src/lib/i18n/translations.js
@@ -2,23 +2,13 @@ import i18n from 'sveltekit-i18n';
 
 /** @type {import('sveltekit-i18n').Config} */
 const config = {
-	loaders: [
-		{
-			locale: 'zh_Hant_HK',
-			key: 'common',
-			loader: async () => (await import('./zh_Hant_HK/common.json')).default
-		},
-		{
-			locale: 'zh_Hant_HK',
-			key: 'station',
-			loader: async () => (await import('./zh_Hant_HK/station.json')).default
-		},
-		{
-			locale: 'zh_Hant_HK',
-			key: 'line',
-			loader: async () => (await import('./zh_Hant_HK/line.json')).default
-		}
-	]
+	loaders: ['en', 'zh_Hant_HK'].flatMap((locale) => {
+		return ['common', 'station', 'line'].map((key) => ({
+			locale: locale,
+			key: key,
+			loader: async () => (await import(`./${locale}/${key}.json`)).default
+		}));
+	})
 };
 
 export const { t, locale, locales, loading, loadTranslations } = new i18n(config);
