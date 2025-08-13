49844cb4f11db3449099b6865443eea42786c577
markmybytes <markmybytes@gmail.com>
2025-08-13 13:26:34 +0800

    Use keword to match fleet table

--- _crawling/main.py
+++ _crawling/main.py
@@ -7,76 +7,76 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 
-crawl_targets = {
+crawl_targets: dict[str, list[dict[str, str]]] = {
     'AEL': [{
         'url': 'https://hkrail.fandom.com/wiki/港鐵機場鐵路ADTranz-CAF列車',
-        'table': 3
+        'keyword': '機場快綫的列車'
     }],
     'DRL': [{
         'url': 'https://hkrail.fandom.com/wiki/港鐵現代化列車',
-        'table': 13
+        'keyword': '迪士尼綫的列車'
     }],
     'EAL': [{
         'url': 'https://hkrail.fandom.com/wiki/港鐵東鐵綫現代列車',
-        'table': 2
+        'keyword': '東鐵綫的列車'
     }],
     'ISL': [
         {
             'url': 'https://hkrail.fandom.com/wiki/港鐵現代化列車#港島綫',
-            'table': 11
+            'keyword': '港島綫的列車'
         },
         {
             'url': 'https://hkrail.fandom.com/wiki/港鐵市區綫中國青島四方製列車#港島綫',
-            'table': 2
+            'keyword': '港島綫的列車'
         },
     ],
     'KTL': [
         {
             'url': 'https://hkrail.fandom.com/wiki/港鐵市區綫中國長春製列車',
-            'table': 1
+            'keyword': '觀塘綫的列車'
         },
         {
             'url': 'https://hkrail.fandom.com/wiki/港鐵市區綫中國青島四方製列車#觀塘綫',
-            'table': 1
+            'keyword': '觀塘綫的列車'
         },
     ],
     'SIL': [{
         'url': 'https://hkrail.fandom.com/wiki/港鐵南港島綫中國長春製列車',
-        'table': 1
+        'keyword': '南港島綫的列車'
     }],
     'TCL': [
         {
             'url': 'https://hkrail.fandom.com/wiki/港鐵機場鐵路ADTranz-CAF列車',
-            'table': 1
+            'keyword': '東涌綫的列車'
         },
         {
             'url': 'https://hkrail.fandom.com/wiki/港鐵市區綫．東涌綫韓製列車',
-            'table': 3
+            'keyword': '東涌綫的列車'
         },
     ],
     'TKL': [
         {
             'url': 'https://hkrail.fandom.com/wiki/港鐵現代化列車#觀塘綫及將軍澳綫',
-            'table': 9
+            'keyword': '觀塘綫及將軍澳綫的列車'
         },
         {
             'url': 'https://hkrail.fandom.com/wiki/港鐵市區綫．東涌綫韓製列車',
-            'table': 1
+            'keyword': '將軍澳綫的列車'
         },
     ],
     'TML': [
         {
             'url': 'https://hkrail.fandom.com/wiki/港鐵屯馬綫中國製列車',
-            'table': 1
+            'keyword': '屯馬綫的列車'
         },
         {
             'url': 'https://hkrail.fandom.com/wiki/港鐵近畿川崎列車',
-            'table': 3
+            'keyword': '屯馬綫的列車'
         },
     ],
     'TWL': [{
         'url': 'https://hkrail.fandom.com/wiki/港鐵現代化列車#荃灣綫',
-        'table': 10
+        'keyword': '荃灣綫的列車'
     }],
 }
 
@@ -97,21 +97,20 @@ def fetch(driver: webdriver.Remote, targets: dict[str, list]):
                 '/')[-1].replace('港鐵', '').split('#')[0]
 
             driver.get(config['url'])
-
             time.sleep(3)
 
-            rows = driver.find_elements(
-                By.XPATH, f'(//table)[{config['table'] + 1}]//tr')
             try:
-                left_dest = driver.find_element(
-                    By.XPATH, f'(//table)[{config['table'] + 1}]//tr[contains(string(.), \'往\')]/td').text
+                table = driver.find_element(By.XPATH, f'//table[.//tr[contains(string(.), "{config['keyword']}")]]')
             except NoSuchElementException:
                 print(f'✗ ...... {line} ({config['url'].split('/')[-1]})')
-                raise RuntimeError('incorrect table')
+                raise RuntimeError(f'Failed to locate table with keyword "{config['keyword']}"')
 
             fleets[line][stock_name] = [
-                clean(l.get_attribute('textContent'), '下行' in left_dest)
-                for l in rows[-1].find_elements(By.CSS_SELECTOR, 'td > ol > li')
+                clean(
+                    l.get_attribute('textContent'), 
+                    '下行' in table.find_element(By.XPATH, './/tr[contains(string(.), "往")]/td').text
+                )
+                for l in table.find_elements(By.XPATH, './/tr[last()]//td/ol/li')
             ]
 
             if (len(fleets[line][stock_name]) == 0):
